5911cf1b4348f48424fc90bb5c2193a7bd99af14
wim <wim@10.136.201.124>
2013-08-01 15:37:14 +0200

    added functionality to start mapping from existing chunks dir for ilumina PE

--- src/bwa_picard_gatk_pipeline/readGroup/ReadGroupIluminaPE.java
+++ src/bwa_picard_gatk_pipeline/readGroup/ReadGroupIluminaPE.java
@@ -13,7 +13,9 @@ import bwa_picard_gatk_pipeline.sge.ilumina.BWAmem.mappingJob.BwaMemIluminaMappi
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
 /**
@@ -24,6 +26,8 @@ public class ReadGroupIluminaPE extends ReadGroupIlumina {
     
     private FastQFile firstReadsFastQFile;
     private FastQFile secondReadsFastQFile;
+    
+    private File fastqChunkDir;
     
     private List<FastQChunk> firstReadsChunks;
     private List<FastQChunk> secondReadsChunks;
@@ -46,7 +50,47 @@ public class ReadGroupIluminaPE extends ReadGroupIlumina {
             secondReadsChunks = secondReadsFastQFile.splitFastQFile(gc.getChunkSize(), readGroupOutputDir,id);
         }  
         
+        if(fastqChunkDir != null)
+        {
+            addExistingChunks();
+        }
+        
     }
+    
+      private void addExistingChunks() {
+        
+           String[] fastqExtensions = new String[] { "fastq", "fq","gz" };
+           List<File> fastqFiles = (List<File>) FileUtils.listFiles(fastqChunkDir, fastqExtensions, true);
+           List<File> existingFirstFileChunks = new ArrayList<File>();
+           List<File> existingSecondFileChunks = new ArrayList<File>();               
+           for (File fastqFile : fastqFiles)  
+           {
+                if(fastqFile.getName().contains("_R1_"))
+                {
+                    existingFirstFileChunks.add(fastqFile);
+                }
+                if(fastqFile.getName().contains("_R2_"))
+                {
+                    existingSecondFileChunks.add(fastqFile);
+                }   
+            }
+           
+           Collections.sort(existingFirstFileChunks);
+           Collections.sort(existingSecondFileChunks);
+           
+           for(File firstFastQFileChunk: existingFirstFileChunks)
+           {
+               firstReadsChunks.add(new FastQChunk(firstFastQFileChunk));
+           }
+           
+           for(File secondFastQFileChunk: existingSecondFileChunks)
+           {
+               secondReadsChunks.add(new FastQChunk(secondFastQFileChunk));
+           }
+           
+    }
+    
+    
 
     @Override
     protected List<Job> createMappingJobs() throws IOException {
@@ -104,6 +148,15 @@ public class ReadGroupIluminaPE extends ReadGroupIlumina {
     public void setSecondReadsFastQFile(FastQFile secondReadsFastQFile) {
         this.secondReadsFastQFile = secondReadsFastQFile;
     }
+
+    public File getFastqChunkDir() {
+        return fastqChunkDir;
+    }
+
+    public void setFastqChunkDir(File fastqChunkDir) {
+        this.fastqChunkDir = fastqChunkDir;
+    }
+    
     
     
     
