f1b8ed3ea2e879ec6d1e6222f232f1ee2c4c6407
wim <wim@localhost>
2013-08-05 17:45:39 +0200

    for Illumina data with existing fastq chunks the check on if the number of reads in fastq and bam are equal is skipped. This is because the reads in the fastq are not known by the program. Maybe parse this from number of reads from bcl converter output.

--- src/bwa_picard_gatk_pipeline/readGroup/ReadGroupIluminaPE.java
+++ src/bwa_picard_gatk_pipeline/readGroup/ReadGroupIluminaPE.java
@@ -23,120 +23,110 @@ import org.apache.commons.io.FilenameUtils;
  * @author wim
  */
 public class ReadGroupIluminaPE extends ReadGroupIlumina {
-    
+
     private FastQFile firstReadsFastQFile;
     private FastQFile secondReadsFastQFile;
-    
     private File fastqChunkDir;
-    private String fastqChunkDirFileNameToMatch;    
-    
-    
+    private String fastqChunkDirFileNameToMatch;
     private List<FastQChunk> firstReadsChunks;
     private List<FastQChunk> secondReadsChunks;
-        
-    
 
     @Override
     protected void prepareReadsForMapping() throws IOException {
-        
+
         firstReadsChunks = new ArrayList<FastQChunk>();
         secondReadsChunks = new ArrayList<FastQChunk>();
-        
-        
-        if(firstReadsFastQFile != null)
-        {
+
+
+        if (firstReadsFastQFile != null) {
             firstReadsFastQFile.initializeFastqReader();
             secondReadsFastQFile.initializeFastqReader();
-            
-            firstReadsChunks = firstReadsFastQFile.splitFastQFile(gc.getChunkSize(), readGroupOutputDir,id);
-            secondReadsChunks = secondReadsFastQFile.splitFastQFile(gc.getChunkSize(), readGroupOutputDir,id);
-        }  
-        
-        if(fastqChunkDir != null)
-        {
+
+            firstReadsChunks = firstReadsFastQFile.splitFastQFile(gc.getChunkSize(), readGroupOutputDir, id);
+            secondReadsChunks = secondReadsFastQFile.splitFastQFile(gc.getChunkSize(), readGroupOutputDir, id);
+        }
+
+        if (fastqChunkDir != null) {
             addExistingChunks();
         }
-        
+
     }
-    
-      private void addExistingChunks() {
-        
-           String[] fastqExtensions = new String[] { "fastq", "fq","gz" };
-           List<File> fastqFiles = (List<File>) FileUtils.listFiles(fastqChunkDir, fastqExtensions, true);
-           List<File> existingFirstFileChunks = new ArrayList<File>();
-           List<File> existingSecondFileChunks = new ArrayList<File>();               
-           for (File fastqFile : fastqFiles)  
-           {
-                //skip files not matching the filename part, in the case that multiple readGroups or lanes are in the same fastqChunkDir
-                if(!fastqFile.getName().contains(fastqChunkDirFileNameToMatch)){ continue;}
-               
-                if(fastqFile.getName().contains("_R1_"))
-                {
-                    existingFirstFileChunks.add(fastqFile);
-                }
-                if(fastqFile.getName().contains("_R2_"))
-                {
-                    existingSecondFileChunks.add(fastqFile);
-                }   
+
+    private void addExistingChunks() {
+
+        String[] fastqExtensions = new String[]{"fastq", "fq", "gz"};
+        List<File> fastqFiles = (List<File>) FileUtils.listFiles(fastqChunkDir, fastqExtensions, true);
+        List<File> existingFirstFileChunks = new ArrayList<File>();
+        List<File> existingSecondFileChunks = new ArrayList<File>();
+        for (File fastqFile : fastqFiles) {
+            //skip files not matching the filename part, in the case that multiple readGroups or lanes are in the same fastqChunkDir
+            if (!fastqFile.getName().contains(fastqChunkDirFileNameToMatch)) {
+                continue;
             }
-           
-           Collections.sort(existingFirstFileChunks);
-           Collections.sort(existingSecondFileChunks);
-           
-           for(File firstFastQFileChunk: existingFirstFileChunks)
-           {
-               firstReadsChunks.add(new FastQChunk(firstFastQFileChunk));
-           }
-           
-           for(File secondFastQFileChunk: existingSecondFileChunks)
-           {
-               secondReadsChunks.add(new FastQChunk(secondFastQFileChunk));
-           }
-           
+
+            if (fastqFile.getName().contains("_R1_")) {
+                existingFirstFileChunks.add(fastqFile);
+            }
+            if (fastqFile.getName().contains("_R2_")) {
+                existingSecondFileChunks.add(fastqFile);
+            }
+        }
+
+        Collections.sort(existingFirstFileChunks);
+        Collections.sort(existingSecondFileChunks);
+
+        for (File firstFastQFileChunk : existingFirstFileChunks) {
+            firstReadsChunks.add(new FastQChunk(firstFastQFileChunk));
+        }
+
+        for (File secondFastQFileChunk : existingSecondFileChunks) {
+            secondReadsChunks.add(new FastQChunk(secondFastQFileChunk));
+        }
+
     }
-    
-    
 
     @Override
     protected List<Job> createMappingJobs() throws IOException {
-        
+
         List<Job> bwaMappingJobs = new ArrayList<Job>();
-        
-        for(int x = 0; x < firstReadsChunks.size(); x++) {
-            
+
+        for (int x = 0; x < firstReadsChunks.size(); x++) {
+
             File bamFile = new File(readGroupOutputDir, FilenameUtils.getBaseName(firstReadsChunks.get(x).getFastqFile().getPath()) + ".bam");
             bamChunks.add(bamFile);
             Job bwaIluminaMappingJob = null;
-            if(gc.getUseBWAMEM())
-            {
-                 bwaIluminaMappingJob = new BwaMemIluminaMappingJob(firstReadsChunks.get(x).getFastqFile(), secondReadsChunks.get(x).getFastqFile(), bamFile, this);      
+            if (gc.getUseBWAMEM()) {
+                bwaIluminaMappingJob = new BwaMemIluminaMappingJob(firstReadsChunks.get(x).getFastqFile(), secondReadsChunks.get(x).getFastqFile(), bamFile, this);
+            } else {
+                bwaIluminaMappingJob = new BwaIluminaMappingJob(firstReadsChunks.get(x).getFastqFile(), secondReadsChunks.get(x).getFastqFile(), bamFile, this);
             }
-            else
-            {
-                 bwaIluminaMappingJob = new BwaIluminaMappingJob(firstReadsChunks.get(x).getFastqFile(), secondReadsChunks.get(x).getFastqFile(), bamFile, this);      
-            }     
-           
+
             bwaMappingJobs.add(bwaIluminaMappingJob);
         }
-       
+
         return bwaMappingJobs;
-        
+
     }
 
     @Override
     protected Long getReadsInChunks() {
-        
-        Long counter = new Long(0);
-        for (FastQChunk fastQChunk : firstReadsChunks) {
-            counter = counter + fastQChunk.getRecordNr();
-        }
-        
-        for (FastQChunk fastQChunk : secondReadsChunks) {
-            counter = counter + fastQChunk.getRecordNr();
+
+        Long counter = new Long(-1);
+
+        //only try to count reads in chunks when there is no existing chunks dir. Otherwise return -1
+        if (fastqChunkDir != null) 
+        {
+            for (FastQChunk fastQChunk : firstReadsChunks) {
+                counter = counter + fastQChunk.getRecordNr();
+            }
+
+            for (FastQChunk fastQChunk : secondReadsChunks) {
+                counter = counter + fastQChunk.getRecordNr();
+            }
         }
 
         return counter;
-    }  
+    }
 
     public FastQFile getFirstReadsFastQFile() {
         return firstReadsFastQFile;
@@ -169,12 +159,4 @@ public class ReadGroupIluminaPE extends ReadGroupIlumina {
     public void setFastqChunkDirFileNameToMatch(String fastqChunkDirFileNameToMatch) {
         this.fastqChunkDirFileNameToMatch = fastqChunkDirFileNameToMatch;
     }
-    
-    
-    
-    
-    
-    
-    
-    
 }
